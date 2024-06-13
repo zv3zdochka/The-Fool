866b5ea993b18ddf885777e87b5998e8232318b0
PUMOVETZ <batsiev2006@gmail.com>
2024-06-13 17:34:47 +0300

    year upd

--- Event.py
+++
@@ -1,8 +0,0 @@
-import time
-class Event:
-    def __init__(self):
-        pass
-    def smt_happened(self):
-        #time.sleep(3)
-
-        return True
--- Suit.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-class Suit:
-    def __init__(self):
-        pass
-
-    def recog_suit(self, image):
-        pass
